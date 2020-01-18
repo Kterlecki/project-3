f157aa9348efc198382893d3bcbb1a674c72c172
Kterlecki <terlecki1995@gmail.com>
2020-01-18 21:51:49 +0000

    connected Mongo and flask

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config ['MONGO_DBNAME'] = 'exercise_manager'
+app.config['MONGO_URI']= 'mongodb+srv://terlek:@project3-f7d5y.mongodb.net/exercise_manager?retryWrites=true&w=majority'
+
+mongo= PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_exercise')
+def get_exercise():
+    return render_template('index.html', tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
