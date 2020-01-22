52d230ff2ad8a2e5ae80ab43c9d5a4c23537dd97
Kterlecki <terlecki1995@gmail.com>
2020-01-22 19:28:45 +0000

    added Procfile and updated base + tasks.html

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
-app.config ['MONGO_DBNAME'] = 'exercise_manager'
-app.config['MONGO_URI']= 'mongodb+srv://terlek:@project3-f7d5y.mongodb.net/exercise_manager?retryWrites=true&w=majority'
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = 'mongodb+srv://terlek:Google123@project3-f7d5y.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo= PyMongo(app)
 
 @app.route('/')
-@app.route('/get_exercise')
-def get_exercise():
-    return render_template('index.html', tasks=mongo.db.tasks.find())
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
